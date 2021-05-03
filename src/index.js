fe6cfb70eb62d9bc4bed52e73884e79060b86559
Harvey Duenas <81559975+devduenas@users.noreply.github.com>
2021-05-03 18:06:42 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePi, triplePi } from "./math.js";
+import * as pi from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
